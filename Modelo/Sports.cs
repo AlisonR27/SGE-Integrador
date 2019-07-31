b1ad532a38e806c99703f882630747dbcf1ba7d0
Raphael Marinho Carvalho <marinho.raphael@academico.ifrn.edu.br>
2019-07-31 17:52:08 -0300

    second commit

--- Modelo/Sports.cs
+++ Modelo/Sports.cs
@@ -7,5 +7,18 @@ namespace WebAppSGE.Modelo
 {
     public class Sports
     {
+        public int id { set; get; }
+        public string nome { set; get; }
+
+        public Sports()
+        {
+            this.id = "";
+            this.nome = "";
+        }
+        public Sports(string a_id, string anome)
+        {
+            this.id = a_id;
+            this.nome = anome;
+        }
     }
 }
