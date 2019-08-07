71dfeb14cadbe5e15db32223bd2243bb358da3b1
Luís Eduardo de Oliveira Sales <luis.sales@academico.ifrn.edu.br>
2019-08-07 16:41:54 -0300

    Alterações Web.config

--- DAL/DALArea.cs
+++ DAL/DALArea.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Configuration;
+using System.Web;
+
 
 namespace WebAppSGE.DAL
 {
+    string connectionString = "";
     public class DALArea
     {
+        connectionString = ConfigurationManager.ConnectionStrings["SGEConnectionString"].ConnectionString;
     }
 }
